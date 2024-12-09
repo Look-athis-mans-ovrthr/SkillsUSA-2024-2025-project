a73c6bb63711a52e4bebb04e79ad8b60071a0050
Look-athis-mans-ovrthr <BrodyMiranda342@riversideunified.org>
2024-12-09 09:24:10 -0800

    updates

--- SkillsUSA Project/Assets/Scripts/PlayerController.cs
+++ SkillsUSA Project/Assets/Scripts/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 20f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
@@ -50,4 +50,7 @@ public class PlayerController : MonoBehaviour
             transform.localScale = localScale;
         }
     }
+
+    {
+        void OnCollisionEnter()}
 }
