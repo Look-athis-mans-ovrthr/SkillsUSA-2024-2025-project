69b21a7fbc83ac36621bf495599e361313a6fec8
AWSEDRFT12 <113384781+AWSEDRFT12@users.noreply.github.com>
2024-12-13 09:21:17 -0800

    done scripting

--- SkillsUSA Project/Assets/Scripts/Coinmanager.cs
+++ SkillsUSA Project/Assets/Scripts/Coinmanager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Coinmanager : MonoBehaviour
 {
     public int coinCount;
+    public TextMeshProUGUI coinText;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,6 @@ public class Coinmanager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        coinText.text = "Gear Count: " + coinCount.ToString();
     }
 }
